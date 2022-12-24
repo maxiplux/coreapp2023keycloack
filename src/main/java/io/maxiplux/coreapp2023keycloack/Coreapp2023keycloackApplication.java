356322e5ff95388ce0c6f7b074b5307aa32a4543
Juan Francisco Mosquera <maxiplux@gmail.com>
2022-12-23 21:23:20 -0500

    Initial commit

---
+++ src/main/java/io/maxiplux/coreapp2023keycloack/Coreapp2023keycloackApplication.java
@@ -0,0 +1,13 @@
+package io.maxiplux.coreapp2023keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coreapp2023keycloackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coreapp2023keycloackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/maxiplux/coreapp2023keycloack/Coreapp2023keycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maxiplux.coreapp2023keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coreapp2023keycloackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
