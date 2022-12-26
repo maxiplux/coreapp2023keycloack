6a0046c9aa3ece57a3e203219dbbc6ddbc4bf3b5
Juan Francisco Mosquera <maxiplux@gmail.com>
2022-12-26 04:36:39 -0500

    Keycloak with library

---
+++ src/main/java/io/maxiplux/coreapp2023keycloack/exeptions/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package io.maxiplux.coreapp2023keycloack.exeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = {InsufficientAuthenticationException.class, Exception.class})
+    public ResponseEntity<ErrorMessage> resourceNotFoundException(InsufficientAuthenticationException ex, WebRequest request) {
+        ErrorMessage message = new ErrorMessage(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+
+        return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+    }
+}
